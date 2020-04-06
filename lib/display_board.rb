27ded4c73db9db390e2117d57fdad2fd5245dc9d
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-04-06 17:52:01 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,14 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} " #row1
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} " #row2
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} " #row3
+  puts
+end
+
+board = [" "," "," "," "," "," "," "," "," "]
+
+display_board(board)
